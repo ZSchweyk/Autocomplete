e91961f8a087b6d6422300de5d1092f422511fb1
Zeyn Schweyk <zeynschweyk@dpengineering.org>
2022-01-27 12:43:07 -0800

    doesn't work. how to print keycode, not the character???

--- auto_complete.py
+++ auto_complete.py
@@ -1,30 +1,82 @@
-from pynput.keyboard import Key, Controller, HotKey
+from pynput.keyboard import Key, Controller, HotKey, KeyCode
 from pynput import keyboard
 import time
 # import keyboard
 
-c = Controller()
 
-def press(button):
-    c.press(button)
-    c.release(button)
-
-
-def parenthesis_autocomplete():
-    with c.pressed(Key.shift):
-        press("9")
-        press("0")
-
-    # c.press(Key.shift)
-    # press("9")
-    # press("0")
-    # c.release(Key.shift)
-    press(Key.left)
+def on_press(key):
+    print(key.code)
+    if key == KeyCode("l"):
+        print("Close parans")
+    # try:
+    #     print('alphanumeric key {0} pressed'.format(
+    #         key.char))
+    # except AttributeError:
+    #     print('special key {0} pressed'.format(
+    #         key))
 
 
 
-with keyboard.GlobalHotKeys({'shift+9': parenthesis_autocomplete}) as h:
-    h.join()
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        ) as listener:
+    listener.join()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# c = Controller()
+#
+# def press(button):
+#     c.press(button)
+#     c.release(button)
+#
+#
+# def parenthesis_autocomplete():
+#     with c.pressed(Key.shift):
+#         press("9")
+#         press("0")
+#
+#     # c.press(Key.shift)
+#     # press("9")
+#     # press("0")
+#     # c.release(Key.shift)
+#     press(Key.left)
+#
+#
+#
+# with keyboard.GlobalHotKeys({'shift+9': parenthesis_autocomplete}) as h:
+#     h.join()
+
+
+
+
+
+
+
+
+
+
+
+
 
 # keyboard.add_hotkey("shift + 9", parenthesis_autocomplete)
 
