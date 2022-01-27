a0ce2fc51cd7f15cc584644688a9c112be3e8396
Zeyn Schweyk <zeynschweyk@dpengineering.org>
2022-01-27 14:47:21 -0800

    Finally! Got it to work!

--- auto_complete.py
+++ auto_complete.py
@@ -1,90 +1,36 @@
 from pynput.keyboard import Key, Controller, HotKey, KeyCode
 from pynput import keyboard
 import time
-# import keyboard
 
 c = Controller()
 
-def press(button, num):
-    for i in range(num):
-        c.press(button)
-        c.release(button)
+def press(button):
+    c.press(button)
+    c.release(button)
 
-def on_press(key):
-    try:
-        key_code = key.vk
-    except AttributeError:
-        key_code = key.value.vk
+def shift_autocomplete(boolean, char):
+    if boolean:
+        with c.pressed(Key.shift_r):
+            press(char)
+    else:
+        press(char)
+    c.release(Key.shift_l)
+    press(Key.left)
 
-    if key_code == 40:
-        # press(Key.backspace, 1)
+with keyboard.GlobalHotKeys(
+        {
+            "<shift>+9": lambda: shift_autocomplete(True, "0"),
 
-        with c.pressed(Key.shift):
-            # press("9", 1)
-            press("0", 1)
-        c.release(Key.shift)
-        press(Key.left, 1)
+            "<shift>+[": lambda: shift_autocomplete(True, "]"),
+            "[": lambda: shift_autocomplete(False, "]"),
 
-
-with keyboard.Listener(
-        on_press=on_press,
-        ) as listener:
-    listener.join()
+            "<shift>+'": lambda: shift_autocomplete(True, "'"),
+            "'": lambda: shift_autocomplete(False, "'"),
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# c = Controller()
-#
-# def press(button):
-#     c.press(button)
-#     c.release(button)
-#
-#
-# def parenthesis_autocomplete():
-#     with c.pressed(Key.shift):
-#         press("9")
-#         press("0")
-#
-#     # c.press(Key.shift)
-#     # press("9")
-#     # press("0")
-#     # c.release(Key.shift)
-#     press(Key.left)
-#
-#
-#
-# with keyboard.GlobalHotKeys({'shift+9': parenthesis_autocomplete}) as h:
-#     h.join()
-
-
-
-
-
-
-
-
-
-
-
-
+        }
+) as h:
+    h.join()
 
 # keyboard.add_hotkey("shift + 9", parenthesis_autocomplete)
 
